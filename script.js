7f84613d87d6ce98a67c744e533e7a99fe79f81d
jeffgemelli <jefferson.gemelli@escola.pr.gov.br>
2025-03-24 23:51:49 +0000

    dsdsds

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let tamanhoAtualFonte = 1;
+
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+    botaoDeAcessibilidade.addEventListener('click', function () {
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+    })
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-
-    aumentaFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte += 0.2;
-        document.body.style.fontSize = ${tamanhoAtualFonte}rem
-    })
-
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-    diminuiFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte -= 0.2;
+    let tamanhoAtualFonte = 1;
+    
+    aumentaFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = ${tamanhoAtualFonte}rem
+
     })
-})
+
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = ${tamanhoAtualFonte}rem
+
+    })
+
+}
+)
